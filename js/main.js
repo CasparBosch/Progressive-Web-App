5175128e75224c4145735e5e95a77eb26cb1828d
CasparBosch <1004288@hr.nl>
2024-02-12 10:20:00 +0100

    < fix

---
+++ js/main.js
@@ -0,0 +1,17 @@
+async function Api() {
+  try {
+    const response = await fetch("https://cmgt.hr.nl/api/projects", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    let jsonfetched = await response.json();
+    console.log(jsonfetched);
+    return jsonfetched.json();
+  } catch {
+    console.error();
+  }
+}
+Api();
